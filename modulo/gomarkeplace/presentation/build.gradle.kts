d16a8aad02cb5d86a4b5a84214b8d29288d2f030
Elisabeth Farias <elisabethfarias7@gmail.com>
2025-07-18 21:14:49 -0300

    feat(gomarketplace): add config lib

--- modulo/gomarkeplace/presentation/build.gradle.kts
+++ modulo/gomarkeplace/presentation/build.gradle.kts
@@ -37,7 +37,16 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    //Gson
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
 }
